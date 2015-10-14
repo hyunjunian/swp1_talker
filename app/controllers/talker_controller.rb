5544b7f875648f47e1ef4edaa54a5558b3cf1e03
hyunjunian <hyunjunian@gmail.com>
2015-10-14 12:24:43 +0000

    lastlast

--- app/controllers/talker_controller.rb
+++ app/controllers/talker_controller.rb
@@ -3,7 +3,28 @@ class TalkerController < ApplicationController
     @chats =Chat.all
   end
   
+  
   def create
+    require 'nokogiri'
+    require 'open-uri'
+    
+    if params[:ask].index("#")==0
+    page = Nokogiri::HTML(open("http://www.fatsecret.kr/%EC%B9%BC%EB%A1%9C%EB%A6%AC-%EC%98%81%EC%96%91%EC%86%8C/search?q="+URI.encode(params[:ask])))
+    cals = page.css('.prominent')
+    Chat.create(chat_type: "user",chat_content: params[:ask])
+    postfix=["칼로리는 ","지방은 ","탄수화물은 ","단백질은 "]
+    cals.each_with_index do |cal,x|
+      break if x==1
+      Chat.create(chat_type: "bot",chat_content: params[:ask]+"의")
+      page2=Nokogiri::HTML(open("http://www.fatsecret.kr/%EC%B9%BC%EB%A1%9C%EB%A6%AC-%EC%98%81%EC%96%91%EC%86%8C"+URI.encode(cal['href'])))
+      cals2=page2.css('.factValue')
+      cals2.each_with_index do |cal2,i|
+      Chat.create(chat_type: "bot",chat_content: postfix[i%4]+cal2.text+" ")
+      end
+      Chat.create(chat_type: "bot",chat_content: "입니다")
+    end
+    redirect_to :root
+    else
     talk=Talk.where(ask: params[:ask]).sample
     
     unless talk.nil?
@@ -13,5 +34,6 @@ class TalkerController < ApplicationController
     else
       redirect_to '/learn'
     end
+  end
   end
 end
